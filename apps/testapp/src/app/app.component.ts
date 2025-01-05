a31341c8aa323f9044ce7c810a674712f9407d49
dormalk <dormalk@gmail.com>
2025-01-05 19:23:07 +0200

    init new app

---
+++ apps/testapp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { NavbarComponent } from '@monorepo-angular/core/navbar';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, NavbarComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'testapp';
+}
